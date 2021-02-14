d26ee00622c0877508a0178dd7f42527fd3c25f7
Hitoshi082508 <hitoshi082508@gmail.com>
2021-02-14 21:14:07 +0900

    よくわからん

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,20 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import reducer from "../reducers"
+import reducer from "../reducers";
+import EventForm from "./EventForm";
+import Events from "./Events";
+import AppContext from "../contexts/AppContext";
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-  const addEvent = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: "CREATE_EVENT",
-      title,
-      body
-    });
-    setTitle("");
-    setBody("");
-  };
 
   return (
-    <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={(e) => setBody(e.target.value)}/>
-        </div>
-      </form>
-
-      <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-      <button className="btn btn-danger">全てのイベントを削除する</button>
-
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
-    </div>
+    <AppContext.Provider value={{state, dispatch}}>
+      <div className="container-fluid">
+        <EventForm/>
+        <Events/>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/Events.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
+import Event from "./Event";
+
+const Events = () => {
+    const { state } = useContext(AppContext);
+    return (
+        <>
+            <h4>イベント一覧</h4>
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>ボディー</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                { state.map((event, index) => (<Event key={index} event={event}/>)) }
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Events;
